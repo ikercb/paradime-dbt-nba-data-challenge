ef9d3198093d9fc4a240c0a869b8058113860e7e
ikercb <62053110+ikercb@users.noreply.github.com>
2024-03-07 09:44:36 +0100

    remove unnecessary code

--- models/season_stats/team_season_stats.sql
+++ models/season_stats/team_season_stats.sql
@@ -12,15 +12,6 @@ WITH team_stats AS (
         Team_PTS
     FROM 
         {{ ref('team_game_advanced_stats') }} 
-    WHERE 
-        --  team_name LIKE '%Cavaliers%'
-        --  AND 
-        --  season = '2008-09'
-        --  team_name LIKE '%Nuggets%'
-        --  AND 
-        --  season = '2022-23'
-        --  AND
-        game_type = 'Regular Season'
 )
 
 SELECT
@@ -29,9 +20,7 @@ SELECT
     team_name,
     team_id,
     SUM(Team_MP) AS mp,
-    COUNT(*) AS games,
     AVG(Team_PACE) AS pace,
-    48 * (SUM(Team_Poss) + SUM(Opp_Poss)) / (2 * SUM(Team_MP) / 5) AS pace_2,
     SUM(Team_PTS) AS total_pts,
     AVG(Team_PTS) AS pts,
     SUM(Team_Poss) AS total_poss,

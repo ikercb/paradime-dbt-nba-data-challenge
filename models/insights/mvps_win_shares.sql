65f1fbb8c41c6b8913bf849eb06d1b695b8ffe76
ikercb <62053110+ikercb@users.noreply.github.com>
2024-03-08 11:18:08 +0100

    add mvps_win_shares.sql

---
+++ models/insights/mvps_win_shares.sql
@@ -0,0 +1,34 @@
+-- Compare the MVP winners with those that got the most win shares in each season.
+WITH player_ws AS (
+    SELECT
+        season,
+        player_name,
+        ws
+    FROM 
+        {{ ref('win_shares') }}
+    -- Keep only the player with the most win shares
+    QUALIFY ROW_NUMBER() OVER(
+        PARTITION BY o.season 
+        ORDER BY ws DESC
+    ) = 1
+),
+mvps AS (
+    SELECT 
+        *
+    FROM 
+        {{ ref('player_awards') }}  
+    WHERE 
+        award = 'NBA Most Valuable Player'
+)
+SELECT 
+    w.player_name AS ws_mvp,
+    m.player_name AS mvp,
+    w.season
+FROM  
+    player_ws AS w
+INNER JOIN 
+    mvps AS m 
+ON 
+    w.season = m.season
+ORDER BY 
+    w.season DESC
